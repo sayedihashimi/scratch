a2f0099b4d870bea784136ac6b309551128bf71a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-09-20 14:20:37 -0400

    adding new project

---
+++ aspnet/MissingRef/MissingRef/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MissingRef {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            IConfiguration foo = null;
+        }
+    }
+}
