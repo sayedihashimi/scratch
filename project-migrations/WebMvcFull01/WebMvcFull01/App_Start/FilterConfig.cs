d6750b5dfc1ef424da33e72ac7d31b0ce77d33d6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-09-26 16:12:15 -0400

    after project create

---
+++ project-migrations/WebMvcFull01/WebMvcFull01/App_Start/FilterConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvcFull01 {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
